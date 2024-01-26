6db67685e8b5548191a794281e68421ee686eeb8
Ruian7P <155224367+Ruian7P@users.noreply.github.com>
2024-01-26 09:38:53 -0500

    Update rollgame.c

--- rollgame.c
+++ rollgame.c
@@ -2,5 +2,28 @@
 
 int main()
 {
-
+  srand(500);
+  int i;
+  for (i=0; i<1000; i++) 
+  {
+    firstroll = rolldice();
+    if firstroll==7 
+    {
+      wins[firstroll]++;
+      continue;
+    }
+    do {
+      roll = rolldice();
+      if roll == 7 {
+        losses[firstroll]++;
+      }
+      else if roll == firstroll
+        wins[firstroll]++;
+      
+    }
+      
+  }
+  
 }
+
+
